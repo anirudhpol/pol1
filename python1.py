4cec5c5ddaebe20acf5669b4a2dbbd14e08b1719
anirudhpol <155996906+anirudhpol@users.noreply.github.com>
2024-01-11 15:23:02 +0530

    Add files via upload
    codes

---
+++ python1.py
@@ -0,0 +1,13 @@
+#even aur odd
+
+num = int (input ("enter a number:"))
+
+if (num % 2):
+
+              print ("The number is even")
+
+elif num == 0:
+       print("zero")
+else:
+    print ("the number is odd")
+
